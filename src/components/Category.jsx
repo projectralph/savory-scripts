22bb93482ebf250e743038d9ea9042b8790a0a11
Rafael Villa <villalugo96@gmail.com>
2022-09-06 02:28:24 -0400

    intitial commit

---
+++ src/components/Category.jsx
@@ -0,0 +1,78 @@
+import {FaPizzaSlice, FaHamburger}  from 'react-icons/fa';
+import {GiNoodles, GiChopsticks} from 'react-icons/gi';
+import styled from 'styled-components';
+import {NavLink} from 'react-router-dom';
+import {motion} from 'framer-motion'
+
+function Category() {
+  return (
+    <List 
+    animate={{opacity: 1}}
+    initial={{opacity: 0}}
+    exit={{opacity: 0}}
+    transition={{duration: 0.5}}
+    
+    >
+        <SLink to={`/cuisine/Italian`}>
+            <FaPizzaSlice />
+            <h4>Italian</h4>
+        </SLink>
+        <SLink to={`/cuisine/American`}>
+            <FaHamburger />
+            <h4>American</h4>
+        </SLink>
+        <SLink to={`/cuisine/Thai`}>
+            <GiNoodles />
+            <h4>Thai</h4>
+        </SLink>
+         <SLink to={`/cuisine/Chinese`}>
+            <GiChopsticks />
+            <h4>Chinese</h4>
+        </SLink> 
+    </List>
+  )
+}
+
+const List = styled(motion.div)`
+display: flex;
+justify-content: center;
+margin: 2rem 0rem;
+`
+
+const SLink = styled(NavLink)`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border-radius: 50%;
+margin-right: 2rem;
+text-decoration: none;
+background: linear-gradient(35deg, #494949, #313131);
+width: 6rem; 
+height: 6rem;
+cursor: pointer;
+transform: scale(0.8);
+
+h4{
+    color: white; 
+    font-size: 0.8rem;
+}
+
+svg{
+    color: white; 
+    font-size: 1.5rem;
+}
+&.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg{
+        color: white;
+    }
+    h4{
+        color: white;
+    }
+}
+
+`
+
+export default Category
